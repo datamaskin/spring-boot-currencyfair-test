300516162646083b06697db0373ce41b052691d0
David Brown <david.brown@contractor.ruckusgaming.com>
2015-04-08 17:21:07 -0500

    Created private method pushDataPoints() to populate data.sparkfun with 50 data points instead of just 1 or 2.

--- src/main/java/jPhant_Test/JPhant_Currency_Fair_Test.java
+++ src/main/java/jPhant_Test/JPhant_Currency_Fair_Test.java
@@ -21,7 +21,7 @@ public class JPhant_Currency_Fair_Test {
         // Configure
         final JPhant jPhant1 = new JPhant(new JPhant_Config_currency_fair());
         JSONStreamParser jsp = null;
-        String json = "{ \"userId\": \"134256\", \"currencyFrom\": \"EUR\", \"currencyTo\": \"GBP\", \"amountSell\": 1000, \"amountBuy\": 747.1, \"rate\": 0.7471, \"timePlaced\": \"24-JAN-15 10:27:44\", \"originatingCountry\": \"FR\" }";
+//        String json = "{ \"userId\": \"134256\", \"currencyFrom\": \"EUR\", \"currencyTo\": \"GBP\", \"amountSell\": 1000, \"amountBuy\": 747.1, \"rate\": 0.7471, \"timePlaced\": \"24-JAN-15 10:27:44\", \"originatingCountry\": \"FR\" }";
         try {
             jsp = new JSONStreamParser("http://localhost:8080/");
         } catch (IOException e) {
@@ -91,8 +91,19 @@ public class JPhant_Currency_Fair_Test {
         System.out.println("\t\tgetXRateLimitRemaining()="+jPhant1.getXRateLimitRemaining());
         System.out.println("\t\tgetXRateLimitReset()="+jPhant1.getXRateLimitReset());
         System.out.println();
+        
+        
+//        try {
+            for (int i=0; i<50; i++) {
+                pushDataPoints(jPhant1, jsp);
+//                Thread.sleep(2000);
+            }
+        /*} catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
 
-        String json1 = jPhant1.getData(JPhant.Format.json);
+        
+        /*String json1 = jPhant1.getData(JPhant.Format.json);
         String[] temp;
         String delims = "[\\[\\],\r\n]+";
         String json2 = null;
@@ -100,7 +111,7 @@ public class JPhant_Currency_Fair_Test {
         if (json1 != null) {
             temp = json1.split(delims);
             int i = 1;
-            while (i<temp.length/2) {
+            while (i<temp.length/2) { // camel case so the POJO doesn't barf
                 if (temp[i].contains("amountbuy")) {
                     String s = replaceCharAt(temp[i], 7, 'B');
                     temp[i] = s;
@@ -145,9 +156,9 @@ public class JPhant_Currency_Fair_Test {
         System.out.println("---------------------------------");
         System.out.println("AddData until PageCount==2 and Verify Page2");
         String sBigData = "";
-        /*while(sBigData.length()<10000) {
+        *//*while(sBigData.length()<10000) {
             sBigData += SDATA[0];
-        }*/
+        }*//*
         try {
             jsp = new JSONStreamParser("http://localhost:8080/update", json2);
         } catch (IOException e) {
@@ -193,9 +204,110 @@ public class JPhant_Currency_Fair_Test {
         System.out.println();
 
         System.out.println("---------------------------------");
+        *//*System.out.println("Clear & Stats");
+        jPhant1.clear();*//*
+        System.out.println("\tStatus="+jPhant1.getStatus().toString());
+        System.out.println();*/
+    }
+    
+    private static void pushDataPoints(JPhant jPhant, JSONStreamParser jsp) {
+
+        String json1 = jPhant.getData(JPhant.Format.json);
+        String[] temp;
+        String delims = "[\\[\\],\r\n]+";
+        String json2 = null;
+        StringBuilder sb = new StringBuilder();
+        if (json1 != null) {
+            temp = json1.split(delims);
+            int i = 1;
+            while (i<9) { // camel case so the POJO doesn't barf
+                if (temp[i].contains("amountbuy")) {
+                    String s = replaceCharAt(temp[i], 7, 'B');
+                    temp[i] = s;
+                } else if (temp[i].contains("amountsell")) {
+                    String s = replaceCharAt(temp[i], 7, 'S');
+                    temp[i] = s;
+                } else if (temp[i].contains("currencyfrom")) {
+                    String s = replaceCharAt(temp[i], 9, 'F');
+                    temp[i] = s;
+                } else if (temp[i].contains("currencyto")) {
+                    String s = replaceCharAt(temp[i], 9, 'T');
+                    temp[i] = s;
+                } else if (temp[i].contains("originatingcountry")) {
+                    String s = replaceCharAt(temp[i], 12, 'C');
+                    temp[i] = s;
+                } else if (temp[i].contains("timeplaced")) {
+                    String s = replaceCharAt(temp[i], 5, 'P');
+                    temp[i] = s;
+                } else if (temp[i].contains("userid")) {
+                    String s = replaceCharAt(temp[i], 6, 'I');
+                    temp[i] = s;
+                }
+//                        case "timestamp" :
+                sb.append(temp[i]);
+                sb.append(",");
+//                System.out.println(temp[i]);
+//                System.out.println(sb.toString());
+                i++;
+            }
+            sb.replace(sb.length()-1, sb.length()-1, "}");
+
+            json2 = sb.toString().substring(0, sb.length()-1);
+        }
+
+        System.out.println("---------------------------------");
+        System.out.println("AddData until PageCount==2 and Verify Page2");
+        
+        try {
+            jsp = new JSONStreamParser("http://localhost:8080/update", json2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] SDATA = jsp.getSDATA();
+        String[] SFIELDS = jsp.getSFIELDS();
+
+        String[][] aasAddData = new String[2][SDATA.length];
+        for (int j=0; j<SDATA.length; j++) {
+            aasAddData[0][j] = SFIELDS[j];
+            aasAddData[1][j] = SDATA[j];
+        }
+
+        boolean bTest = jPhant.addData(aasAddData);
+        System.out.println("\tbTest="+bTest);
+        System.out.println("\tAddData: "+jPhant.toStringArrayListOfStringArrayLists(aasAddData).toString());
+
+        String[][] aasReturnData = jPhant.getData();
+        for(String[] asRow : aasReturnData) {
+            for(String sField : asRow) {
+                System.out.print(sField+", ");
+            }
+            System.out.println();
+        }
+
+        JPhant.Stats sStats = jPhant.getStatus();
+        System.out.println("\tTest stats: " + sStats);
+
+        aasReturnData = jPhant.getData();
+        System.out.println("\tRcvd:'"+jPhant.toStringArrayListOfStringArrayLists(aasReturnData).toString()+"'");
+
+        System.out.println("\tGetData: "+jPhant.toStringArrayListOfStringArrayLists(aasReturnData).toString());
+        System.out.println("\tVerify AddData & GetData are identical above (w/ addition of timstamp)...");
+        System.out.println("\tData Identical="+(aasAddData[0][0].equals(aasReturnData[0][0]) && aasAddData[0][1].equals(aasReturnData[0][1]) && aasAddData[1][0].equals(aasReturnData[1][0]) && aasAddData[1][1].equals(aasReturnData[1][1])));
+
+        System.out.println("\tStatus="+jPhant.getStatus().toString());
+        System.out.println("\tVerify PageCount==1 and Used>0 above...");
+
+        System.out.println("\tTest Get Rate Limits...");
+        System.out.println("\t\tgetXRateLimitLimit()="+jPhant.getXRateLimitLimit());
+        System.out.println("\t\tgetXRateLimitRemaining()="+jPhant.getXRateLimitRemaining());
+        System.out.println("\t\tgetXRateLimitReset()="+jPhant.getXRateLimitReset());
+        System.out.println();
+
+        System.out.println("---------------------------------");
         /*System.out.println("Clear & Stats");
         jPhant1.clear();*/
-        System.out.println("\tStatus="+jPhant1.getStatus().toString());
+        System.out.println("\tStatus="+jPhant.getStatus().toString());
         System.out.println();
+
     }
 }
